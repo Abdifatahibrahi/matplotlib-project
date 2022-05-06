02cdde455f3a3f60035d8514bc18e88f243220cb
Abdifatah <alsidiq099@gmail.com>
2022-05-06 16:07:05 +0300

    Added new row

--- matplot.py
+++ matplot.py
@@ -1,15 +1,20 @@
 from matplotlib import pyplot as plt
 
 
-dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 68748, 73752]
 
-plt.plot(dev_x, dev_y)
+plt.plot(ages_x, dev_y)
 
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370]
+
+plt.plot(ages_x, py_dev_y)
 plt.xlabel('Ages')
 plt.ylabel('Median Salary(USD)')
 
 plt.title('Median Salary (USD) by Age')
+
+plt.legend(['All Devs', 'Python'])
 
 plt.show()
