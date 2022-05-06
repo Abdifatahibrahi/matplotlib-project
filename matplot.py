dd7fa268209376a90ed26267483d801a8ccd71a4
Abdifatah <alsidiq099@gmail.com>
2022-05-06 17:15:24 +0300

    Added colors and markers to the graph

--- matplot.py
+++ matplot.py
@@ -1,16 +1,18 @@
 from cProfile import label
+from turtle import color
 from matplotlib import pyplot as plt
+
 
 
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 68748, 73752]
 
-plt.plot(ages_x, dev_y, label='All Devs')
+plt.plot(ages_x, dev_y, color='k', linestyle='--', marker='.', label='All Devs')
 
 py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370]
 
-plt.plot(ages_x, py_dev_y, label='Python')
+plt.plot(ages_x, py_dev_y, color='b', label='Python', marker='o',)
 plt.xlabel('Ages')
 plt.ylabel('Median Salary(USD)')
 
