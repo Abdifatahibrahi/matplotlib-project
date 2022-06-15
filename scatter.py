c1f6f5901584c2480583af4e2144e5d16d3dc401
Abdifatah Abdi <alsidiq099@gmail.com>
2022-06-14 21:22:47 -0700

    Added a scatter plot graph

---
+++ scatter.py
@@ -0,0 +1,23 @@
+
+# import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
+
+x = [5,7,8,5,6,7,9,2,3,4,4,4,2,6,3,6,8,6,4,1]
+y = [7,4,3,9,1,3,2,5,2,4,8,7,1,6,4,9,7,7,5,1]
+
+
+plt.scatter(x,y, s=100, c='green', edgecolors='black', linewidths=1, alpha=0.75)
+
+
+plt.legend()
+
+plt.title('Ages of respondents')
+plt.xlabel('Ages')
+plt.ylabel('Total Respondents')
+
+plt.tight_layout()
+
+plt.show()
+
